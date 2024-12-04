5de2d17d70fc6ab7b4ad299ba0d5771d03ecfd0a
Gianluca Filippini <gianluca.filippini@ebv.com>
2024-12-04 11:01:58 +0100

    wip

--- bin/astrial-lock.sh
+++ bin/astrial-lock.sh
@@ -3,7 +3,7 @@
 if test -f /tmp/amon_lock; then
    USR=`cat /tmp/amon_lock`
    echo
-   echo "[ERROR] Hailo hw already locked by: $USR"
+   echo "[ERROR] Board hw already locked by: $USR"
    echo
 else
    echo `whoami` > /tmp/amon_lock
